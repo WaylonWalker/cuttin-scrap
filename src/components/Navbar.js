16e09117992eb705d9afb48736493090466fe2cb
Waylon S Walker <quadmx08@gmail.com>
2018-09-23 11:00:25 -0500

    updated navigation menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
+  <nav>
         <Link to="/" className="navbar-item">
           <figure className="image">
             < img src = 'https://res.cloudinary.com/cuttin-scrap/image/upload/c_scale,w_150/v153753233/logo/cuttinscrap.png'
@@ -14,25 +10,34 @@ const Navbar = () => (
             />
           </figure>
         </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </div>
-    </div>
+        <ul>
+          <li>
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/project">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/blog">
+              blog
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/giving-back">
+              Giving Back
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar-item" to="/contact">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        
   </nav>
 )
 
