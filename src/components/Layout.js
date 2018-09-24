37f9bd0b3be79a2d80b0bc575eaf47d67b22cafd
Waylon S Walker <quadmx08@gmail.com>
2018-09-23 23:25:37 -0500

    added meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,18 @@ import './sass/main.sass'
 
 const TemplateWrapper = ({ children }) => (
   <>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet 
+      title="Home | Gatsby + Netlify CMS" 
+    >
+      <meta name="twitter:site" content="@cuttinscrap" />
+      <meta name="og:type" content="website" />
+      <meta name="og:site_name" content="CuttinScrap" />
+      < meta name = "theme-color"
+      content = "#0494B1"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <html lang='en' />
+    </Helmet>
+
     <Navbar />
     <div id='content'>{children}</div>
   </>
