5fb3be9a3ff53fb0d688f99ce61d32ecfd2694fe
Waylon S Walker <quadmx08@gmail.com>
2018-09-26 23:15:47 -0500

    improved project style

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -5,6 +5,11 @@ import ProjectGalleryItem from "../components/ProjectGalleryItem";
 const ProjectGallery = props => {
   return (
     <div className="projectGalleryWrapper">
+      <div className="projectTitleWrapper">
+        <div className="bar" />
+        <h1>Projects</h1>
+        <div className="bar" />
+      </div>
       <section className="projectGallery">
         {props.posts.map(({ node: post }) => (
           <ProjectGalleryItem post={post} key={post.id} />
