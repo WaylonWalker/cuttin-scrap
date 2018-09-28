a2fc148d21cdca8a6be9b8588e5538ecea073436
Waylon S Walker <quadmx08@gmail.com>
2018-09-27 23:10:24 -0500

    mobile navbar hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
-const Navbar = () => (
-  <nav>
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.nav_open = this.nav_open.bind(this);
+    this.state = {
+      active: false
+    }
+  };
+
+  nav_open() {
+    const currentState = this.state.active
+    this.setState({ active: !currentState })
+  }
+
+  render() {
+    return (
+      <nav>
         <Link to="/" alt='cuttin-scrap' className="navbar-item">
-          <figure className="image">
-            < img src = '/img/cuttinscrap_w_150.png'
-            alt = "cuttin-scrap"
+          <figure className="logo">
+            < img src='/img/cuttinscrap_w_150.png'
+              alt="cuttin-scrap"
             />
           </figure>
         </Link>
-        <ul>
+        <button
+          id="ham-open"
+          // className={this.state.active ? 'open' : 'closed'}
+          onClick={this.nav_open}
+        >
+          ☰
+        </button>
+        <ul className={this.state.active ? 'open' : 'closed'}>
           <li>
             <Link className="navbar-item" to="/">
               Home
@@ -42,8 +65,10 @@ const Navbar = () => (
             </Link>
           </li>
         </ul>
-        
-  </nav>
-)
 
-export default Navbar
+      </nav >
+
+    )
+  }
+}
+
