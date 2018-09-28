271879c536cf84d5b49172527289a0fce550ae4a
Waylon S Walker <quadmx08@gmail.com>
2018-09-27 23:09:41 -0500

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "gatsby";
+import ProjectGalleryItem from "../components/ProjectGalleryItem";
+
+const ProjectGallery = props => {
+  return (
+    <footer>
+      <em>share your scraps with us</em>
+      <h3>social media</h3>
+      <h3>contact</h3>
+      <h3>metrics</h3>
+    </footer>
+  );
+};
+
+export default ProjectGallery;
