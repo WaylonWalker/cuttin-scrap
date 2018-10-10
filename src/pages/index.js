5c9c2e0498f6ff0375cb6b62631c1f7e96d2837e
Waylon S Walker <quadmx08@gmail.com>
2018-10-09 21:35:16 -0500

    added blogs to grapgql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,31 +8,53 @@ import BlogGallery from "../components/BlogGallery";
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+    const { projects, blogs } = data
 
     return (
       <Layout>
         <div className="hero" />
-        <ProjectGallery posts={posts} />
-        <BlogGallery/>
+        <ProjectGallery posts={projects.edges} />
+        <BlogGallery posts={blogs.edges} />
       </Layout>
     );
   }
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
-};
+// IndexPage.propTypes = {
+//   data: PropTypes.shape({
+//     allMarkdownRemark: PropTypes.shape({
+//       edges: PropTypes.array
+//     })
+//   })
+// };
 
 export const pageQuery = graphql`
   query ProjectPageQuery {
-    allMarkdownRemark(
+    projects: allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       filter: { frontmatter: { templateKey: { eq: "project-post" } } }
+      limit: 10
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+            thumbnailImage
+          }
+        }
+      }
+    }
+  
+      blogs: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      limit: 10
     ) {
       edges {
         node {
