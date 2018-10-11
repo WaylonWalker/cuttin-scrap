e76da2a46dc2aea97e8c95f887450fad20cdc764
Waylon S Walker <quadmx08@gmail.com>
2018-10-10 23:35:44 -0500

    adjusting meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,28 +11,30 @@ const TemplateWrapper = ({ children, site_name, title, theme_color, description,
       title="CuttinScrap"
     >
       <html lang='en' />
-      <meta name="description" content={description}/>
+      <meta name="description" content={description} />
+
       <meta property="og:site_name" content={site_name} />
-      <meta property="og:title" content={title}/>
-      <meta name="og:type" content={type} />
-      <meta name="og:url" content={url}/>
-      <meta name="og:image" content={image}/>
-      <meta name="og:description" content={description}/>
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:description" content={description} />
 
       <meta property="article:published_time" content={time} />
       <meta property="article:modified_time" content={time} />
 
-      <meta itemprop="name" content={title}/>
-      <meta itemprop="description" content={description}/>
-      <meta itemprop="image" content={image}/>
+      <meta itemprop="name" content={title} />
+      <meta itemprop="description" content={description} />
+      <meta itemprop="image" content={image} />
 
-      <meta name="twitter:card" content={card}/>
+      <meta name="twitter:card" content={card} />
       <meta name="twitter:site" content={twitter} />
-      <meta name="twitter:title" content={title}/>
-      <meta name="twitter:description" content={description}/>
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:text:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <meta name="twitter:creator" content={twitter} />
-      <meta name="twitter:image" content={image} />
-      
+      <meta name="twitter:image:src" content={image} />
+
       <meta property="fb:admins" content={fb_app_id} />
 
       <meta name="theme-color" content={theme_color} />
@@ -54,7 +56,7 @@ const TemplateWrapper = ({ children, site_name, title, theme_color, description,
 
 TemplateWrapper.defaultProps = {
   site_name: 'Cuttin Scrap',
-  title: "Let's Start Cuttin Scrap",
+  title: "Lets Start Cuttin Scrap",
   theme_color: "#0494B1",
   description: "When crafting from the heart, perfection is not the main goal. Here at Cuttin Scrap I push towards having fun and using what you have around the house. These projects are not meant for Muffy McPerfect Pants, but for the everyday crafter. I, Rhiannon Walker, fight Atypical Carcinoid Cancer of the lungs everyday, and use crafting as my way to let loose. Come on a journey with me, where the road is never straigh",
   keywords: "YouTube,  Blog, Quilting, Quilts, Sewing",
